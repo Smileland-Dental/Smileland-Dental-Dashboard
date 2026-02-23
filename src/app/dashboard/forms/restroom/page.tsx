904732149ef89b99ad47dc668ba0aae0d9fad5fe
Iris Yim <hyim@smlnd.com>
2026-02-23 09:40:11 -0800

    Update page.tsx

--- src/app/dashboard/forms/restroom/page.tsx
+++ src/app/dashboard/forms/restroom/page.tsx
@@ -54,7 +54,7 @@ export default function RestroomInspection() {
     "California": ["Kindal"],
     "Delano": ["Helen", "Stephanie"],
     "Fresno": ["Cynthia"],
-    "Ming": ["Kindal"],
+    "Ming": ["Kindal", "Hopie"],
     "Ortho": ["Kindal"],
     "Tulare": ["Dianne"],
     "Visalia": ["Dianne"]
@@ -1131,3 +1131,4 @@ export default function RestroomInspection() {
     </>
   );
 }
+
