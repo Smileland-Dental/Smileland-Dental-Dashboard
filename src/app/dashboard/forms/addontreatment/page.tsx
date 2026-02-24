5eb10b5387d14428f4ed8c9e17fea17ca2fbf4cc
Iris Yim <hyim@smlnd.com>
2026-02-24 10:46:07 -0800

    Update page.tsx

--- src/app/dashboard/forms/addontreatment/page.tsx
+++ src/app/dashboard/forms/addontreatment/page.tsx
@@ -798,7 +798,7 @@ export default function AddOnTreatment() {
 
         const userData = userDoc.data();
 
-        if (userData?.role !== 'MANAGER') {
+        if (userData?.role !== 'MANAGER' && userData?.role !== 'USER') {
           alert('You do not have access to this page.');
           setIsAuthorized(false);
           // 다른 페이지로 리다이렉트하거나 홈으로 이동
@@ -1242,5 +1242,6 @@ export default function AddOnTreatment() {
     </>
   );
 }
+
 
 
