a9d644374319167ed1ea8c6955807cb425561fc8
Iris Yim <hyim@smlnd.com>
2026-02-24 15:22:18 -0800

    Update page.tsx

--- src/app/dashboard/forms/supply-manager/page.tsx
+++ src/app/dashboard/forms/supply-manager/page.tsx
@@ -804,7 +804,7 @@ function SupplyManagerSystemContent() {
 
         const userData = userDoc.data();
 
-        if (userData?.role !== 'Manager' && userData?.role !== 'User') {
+        if (userData?.role !== 'Manager' && userData?.role !== 'Employee') {
           alert('You do not have access to this page.');
           setIsAuthorized(false);
           if (typeof window !== 'undefined') {
@@ -1554,5 +1554,6 @@ export default function SupplyManagerSystem() {
     </>
   );
 }
+
 
 
