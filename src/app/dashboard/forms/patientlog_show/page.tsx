f7cdc5c763ea8e7e997652927fec8d8bb4d947e7
Iris Yim <hyim@smlnd.com>
2026-02-24 11:35:02 -0800

    Update page.tsx

--- src/app/dashboard/forms/patientlog_show/page.tsx
+++ src/app/dashboard/forms/patientlog_show/page.tsx
@@ -273,7 +273,7 @@ export default function ShowCheckSystem() {
 
         const userData = userDoc.data();
 
-        if (userData?.role !== 'MANAGER' && userData?.role !== 'USER') {
+        if (userData?.role !== 'Manager' && userData?.role !== 'User') {
           alert('You do not have access to this page.');
           setIsAuthorized(false);
           // 다른 페이지로 리다이렉트하거나 홈으로 이동
@@ -1360,4 +1360,5 @@ export default function ShowCheckSystem() {
     </>
   );
 }
+
 
