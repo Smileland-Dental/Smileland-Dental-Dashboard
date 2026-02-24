8d2ac0788d9d68133a114a8e5b70b2b284ac410e
Iris Yim <hyim@smlnd.com>
2026-02-24 15:16:21 -0800

    Update page.tsx

--- src/app/dashboard/forms/patientlog/page.tsx
+++ src/app/dashboard/forms/patientlog/page.tsx
@@ -932,7 +932,7 @@ function PatientLogSystem(): React.ReactElement {
 
         const userData = userDoc.data();
 
-        if (userData?.role !== 'Manager' && userData?.role !== 'User') {
+        if (userData?.role !== 'Manager' && userData?.role !== 'Employee') {
           alert('You do not have access to this page.');
           setIsAuthorized(false);
           // 다른 페이지로 리다이렉트하거나 홈으로 이동
@@ -1944,6 +1944,7 @@ function PatientLogSystem(): React.ReactElement {
 export default function PatientLogPage() {
   return <PatientLogSystem />;
 }
+
 
 
 
