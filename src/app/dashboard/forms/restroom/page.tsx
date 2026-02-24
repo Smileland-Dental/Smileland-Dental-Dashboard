b0c8b4e2e37f105a1333db57409524b0061f6027
Iris Yim <hyim@smlnd.com>
2026-02-24 15:14:55 -0800

    Update page.tsx

--- src/app/dashboard/forms/restroom/page.tsx
+++ src/app/dashboard/forms/restroom/page.tsx
@@ -735,7 +735,7 @@ export default function RestroomInspection() {
 
         const userData = userDoc.data();
 
-        if (userData?.role !== 'Manager' && userData?.role !== 'User') {
+        if (userData?.role !== 'Manager' && userData?.role !== 'Employee') {
           alert('You do not have access to this page.');
           setIsAuthorized(false);
           // 다른 페이지로 리다이렉트하거나 홈으로 이동
@@ -1131,6 +1131,7 @@ export default function RestroomInspection() {
     </>
   );
 }
+
 
 
 
